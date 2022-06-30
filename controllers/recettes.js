ab49134128ee54b16449a6bb7f05860d13de4509
saleem51 <saleem51@hotmail.fr>
2022-06-30 14:38:28 +0200

    put route

--- controllers/recettes.js
+++ controllers/recettes.js
@@ -1,7 +1,5 @@
 const Recette = require('../models/Recettes');
-const ejs = require('ejs');
 let recettes;
-
 
 
 const postRecettes = async (req, res) => {
@@ -14,13 +12,26 @@ const postRecettes = async (req, res) => {
     });
     try{
         const recetteSaved = await recette.save();
-        console.log(recetteSaved);
+        //console.log(recetteSaved);
         res.send(recetteSaved)
     } catch (err) {
         res.status(400).send(err);
         console.log(err);
     }
 };
+
+const modifyRecette =  async (req, res) => {
+    const recette = await Recette.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id });
+    try{
+        const recetteUpdated = await recette.save();
+        console.log(recetteUpdated);
+        res.send(recetteUpdated);
+    } catch (err) {
+        res.status(400).send(err);
+        console.log(err);
+    }
+
+}
 
 // const findRecettes = (req, res) => {
 //     Recette.find()
@@ -32,9 +43,7 @@ const showRecettes = (req, res) => {
     Recette.find()
     .then(recette => {
         recettes = recette;
-        res.render('recettes', {
-            recettes : recettes
-        });
+        res.render('recettes', { recettes });
     })
     .catch(error => res.status(400).send(error));
   
@@ -44,11 +53,16 @@ const getFormRecettes = (req, res) => {
     res.render('postRecettes')
 }
 
+const getFormPutRecette = (req, res) => {
+    res.render('putRecettes', { recettes })
+}
 
 
 module.exports = {
     // findRecettes,
     postRecettes,
     showRecettes,
-    getFormRecettes
+    getFormRecettes,
+    modifyRecette,
+    getFormPutRecette
 }
--- routes/recettes.js
+++ routes/recettes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {postRecettes, showRecettes, getFormRecettes } = require('../controllers/recettes');
+const {postRecettes, showRecettes, getFormRecettes, modifyRecette, getFormPutRecette } = require('../controllers/recettes');
 const router = express.Router();
 const multer = require('../middleware/multer-config');
 
@@ -7,6 +7,8 @@ const multer = require('../middleware/multer-config');
 // router.get('/', findRecettes);
 router.get('/', showRecettes);
 router.post('/recette/post', multer, postRecettes);
+router.get(`/recette/put/:id`, getFormPutRecette);
+router.put('/recette/put/:id', multer, modifyRecette);
 router.get('/recette/post', getFormRecettes);
 
 module.exports = router;
